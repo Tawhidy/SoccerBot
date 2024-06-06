611d80796d38440ebd0d714e131b423a7cb990bd
Tawhid Alam <108300293+TawhidyRowdy@users.noreply.github.com>
2024-06-06 13:44:00 -0700

    Update Soccerbot_code_finished.ino
    Added 45 degrees option with more efficient algorithm

--- Necessary_Codes/Soccerbot_code_finished.ino
+++ Necessary_Codes/Soccerbot_code_finished.ino
@@ -6,9 +6,6 @@ IBusBM IBus;     //create IBus object:
 int CH1 = 0; 
 int CH2 = 0; 
 int THROTTLE = 0; 
-//int CH4 = 0; 
-//int CH5 = 0; 
-//int CH6 = 0;
 
 //variables
 int L_fwd = 5;
@@ -35,13 +32,13 @@ int bac_end = 1000;
 //RIGHT RANGES
 int ch1_right_end = 2000;
 int ch1_right_start = 1510;
-int ch2_right_start = 1490;
-int ch2_right_end = 1000;
+int ch2_right_start = 1510;
+int ch2_right_end = 2000;
 //LEFT RANGES
 int ch1_left_end = 1000;
 int ch1_left_start = 1490;
-int ch2_left_start = 1510;
-int ch2_left_end = 2000;
+int ch2_left_start = 1490;
+int ch2_left_end = 1000;
 
 
 //Throttle ranges
@@ -66,22 +63,14 @@ void loop(){
   CH1 = IBus.readChannel(0);
   CH2 = IBus.readChannel(1);
   THROTTLE = IBus.readChannel(2);
-  //CH4 = IBus.readChannel(3);
-  //CH5 = IBus.readChannel(4);
-  //CH6 = IBus.readChannel(5);
-  
-  Serial.print(CH1);
-  Serial.print(",1 ");
-  Serial.print(CH2);
-  Serial.print(",2 ");
-  Serial.println(THROTTLE);
-  //Serial.print(",3 ");
-  //Serial.print(CH4);
-  //Serial.print(",4 ");
-  //Serial.print(CH5);
-  //Serial.print(",5 ");
-  //Serial.println(CH6);
 
+  
+  //Serial.print(CH1);
+  //Serial.print(",1 ");
+  //Serial.print(CH2);
+  //Serial.print(",2 ");
+  //Serial.println(THROTTLE);
+ 
 
 //5V for enable pins
 digitalWrite(R1_EN, HIGH);
@@ -95,29 +84,23 @@ digitalWrite(L2_IS, HIGH);
 
 //speed mapping
 int MaxSpeed = map(THROTTLE, throttle_start, throttle_end, 0, 255);
-int spd_fwd = map(CH2, fwd_start, fwd_end, 0, MaxSpeed); //FWD 
-int spd_bac = map(CH2, bac_start, bac_end, 0, MaxSpeed); //BAC
-int ch1_spd_right = map(CH1, ch1_right_start, ch1_right_end, 0, MaxSpeed); //RIGHT ch1
-int ch2_spd_right = map(CH2, ch2_right_start, ch2_right_end, 0, MaxSpeed); //RIGHT ch2
-int ch1_spd_left = map(CH1, ch1_left_start, ch1_left_end, 0, MaxSpeed); //LEFT ch1
-int ch2_spd_left = map(CH2, ch2_left_start, ch2_left_end, 0, MaxSpeed); //LEFT ch1
+int ch1_spd_high = map(CH1, ch1_right_start, ch1_right_end, 0, MaxSpeed); //RIGHT ch1
+int ch2_spd_high = map(CH2, ch2_right_start, ch2_right_end, 0, MaxSpeed); //RIGHT ch2
+int ch1_spd_low = map(CH1, ch1_left_start, ch1_left_end, 0, MaxSpeed); //LEFT ch1
+int ch2_spd_low = map(CH2, ch2_left_start, ch2_left_end, 0, MaxSpeed); //LEFT ch1
 
 
 
 Serial.print(MaxSpeed);
 Serial.print(",max ");
-Serial.print(spd_fwd);
-Serial.print(",fwd ");
-Serial.print(spd_bac);
-Serial.print(",bac ");
-Serial.print(ch1_spd_right);
-Serial.print(",ch1_right ");
-Serial.print(ch2_spd_right);
-Serial.print(",ch2_right ");
-Serial.print(ch1_spd_left);
-Serial.print(",ch1_spd_left ");
-Serial.print(ch2_spd_left);
-Serial.println(",ch2_left ");
+Serial.print(ch1_spd_high);
+Serial.print(",ch1_high ");
+Serial.print(ch2_spd_high);
+Serial.print(",ch2_high ");
+Serial.print(ch1_spd_low);
+Serial.print(",ch1_spd_low ");
+Serial.print(ch2_spd_low);
+Serial.println(",ch2_low ");
 
 
 
@@ -128,43 +111,62 @@ analogWrite(R_fwd, 0);
 analogWrite(R_bac, 0);
 }
 
+//ForWard
+else if((CH2 > fwd_start)&&( 1510 > CH1 > 1490 )){
 
-//FORWARD
-else if(CH1>fwd_start){     
-analogWrite(L_fwd, spd_fwd);
-analogWrite(L_bac, 0);
-analogWrite(R_fwd, spd_fwd);
-analogWrite(R_bac, 0); 
-}
-
-
-//RIGHT
-else if((CH1>ch1_right_start)&&(CH2<ch2_right_start)){
-
-analogWrite(L_fwd, ch1_spd_right);
-analogWrite(L_bac, 0);
-analogWrite(R_fwd, 0);
-analogWrite(R_bac, ch2_spd_right);
-}
-
-
-//LEFT
-else if((CH1<ch1_left_start)&&(CH2>ch2_left_start)){ 
- 
 analogWrite(L_fwd, 0);
-analogWrite(L_bac, ch1_spd_left);
-analogWrite(R_fwd, ch2_spd_left);
+analogWrite(L_bac, ch2_spd_high);
+analogWrite(R_fwd, 0);
+analogWrite(R_bac, ch2_spd_high); 
+}
+
+
+//TOP-RIGHT
+else if(( CH1 > 1510 )&&( CH2 > 1510 )){
+
+analogWrite(L_fwd, ch1_spd_high);
+analogWrite(L_bac, 0);
+analogWrite(R_fwd, ch2_spd_high);
 analogWrite(R_bac, 0);
 }
 
 
-//BACKWARD
-else if(CH1<bac_start) // here channel 1 and 2 has same reading
-{
+//TOP_LEFT
+else if((CH1 < 1490 )&&(CH2 > 1510 )){
+
+analogWrite(L_fwd, ch2_spd_high);
+analogWrite(L_bac, 0);
+analogWrite(R_fwd, ch1_spd_low);
+analogWrite(R_bac, 0);
+}
+
+
+//BOTTOM-RIGHT
+else if(( CH1 > 1510 )&&( CH2 < 1490 )){ 
+ 
 analogWrite(L_fwd, 0);
-analogWrite(L_bac, spd_bac);
+analogWrite(L_bac, ch2_spd_high);
 analogWrite(R_fwd, 0);
-analogWrite(R_bac, spd_bac); 
+analogWrite(R_bac, ch1_spd_low);
+}
+
+
+//BOTTOM-LEFT
+else if((CH1 < 1490 )&&(CH2 < 1490 )){ 
+ 
+analogWrite(L_fwd, 0);
+analogWrite(L_bac, ch2_spd_low);
+analogWrite(R_fwd, 0);
+analogWrite(R_bac, ch1_spd_low);
+}
+
+
+//BACKWARD
+else if((CH2 < bac_start)&&(1510 > CH1 > 1490)){
+analogWrite(L_fwd, 0);
+analogWrite(L_bac, ch2_spd_low);
+analogWrite(R_fwd, 0);
+analogWrite(R_bac, ch2_spd_low); 
 }
 
 
@@ -174,4 +176,5 @@ analogWrite(L_bac, 0);
 analogWrite(R_fwd, 0);
 analogWrite(R_bac, 0);
 }
+
 }
